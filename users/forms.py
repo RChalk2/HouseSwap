cf0ae0c395782ac49ff413de94fef3ad19feb6a7
RChalk2 <116763759+RChalk2@users.noreply.github.com>
2023-10-11 17:23:14 +0100

    add email to registration form

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2"]
 
 
 class LoginForm(forms.Form):
