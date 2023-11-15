267822352b1abf606d8fe2a1db835b543a930eaf
RChalk2 <116763759+RChalk2@users.noreply.github.com>
2023-11-15 17:18:54 +0000

    add comments and tidy up code

--- HouseSwap/settings.py
+++ HouseSwap/settings.py
@@ -148,3 +148,14 @@ STATIC_URL = "/static/"
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
