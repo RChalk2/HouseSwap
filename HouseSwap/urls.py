088db858468d143f3c036bffbf687d689e1e9533
RChalk2 <116763759+RChalk2@users.noreply.github.com>
2023-11-16 09:26:04 +0000

    different settings file for dev and production

--- HouseSwap/urls.py
+++ HouseSwap/urls.py
@@ -39,7 +39,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # I will use Django  to serve my pictures even in production
     # This is not recommended - it seems I should really set up  some other
